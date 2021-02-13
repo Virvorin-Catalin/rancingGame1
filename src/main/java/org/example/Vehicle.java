4e2909c590ead9dc62c5d7a80d54d2014b67e41c
Virvorin Catalin <catalinvirvorin@gmail.com>
2021-02-13 18:09:39 +0200

    studied methods

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -9,6 +9,28 @@ public class Vehicle {
     double mileage;
     String color;
     double fuelLevel;
+    double totalDistance;
     boolean damaged;
 
+   public  double accelerate(double speed, double durationInHours) {
+// Audi is accelerating whit 60 km/h for 1h
+       System.out.println(name + "is accelerating whit " + speed + " km/h for " + durationInHours + " h");
+
+     // local variable
+      double traveledDistance = speed * durationInHours;
+       System.out.println("Traveled distance: " + traveledDistance);
+
+       totalDistance += traveledDistance;
+
+       System.out.println("Total traveled distance: " + totalDistance);
+
+       double usedFuel = traveledDistance * mileage / 100;
+
+       fuelLevel = fuelLevel - usedFuel;
+       //same result as the instruction above
+//       fuelLevel -= usedFuel;
+
+       System.out.println("Remaining fuel: " + fuelLevel);
+       return traveledDistance;
+   }
 }
